bc922e7efd901e66b10cda44f404cebb82e042fd
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-16 18:14:29 -0500

    edited

--- js/game.js
+++ js/game.js
@@ -1,3 +1,19 @@
 let context = document.getElementById("gCanvas").getContext("2d")
 context.canvas.width = 300
 context.canvas.height = 700
+
+let fps = 60
+
+let bird = {
+    x: 100,
+    y: 150,
+    w: 50,
+    h: 50
+}
+
+setInterval(loop, 1000 / fps)
+
+function loop(){
+    context.clearRect(0, 0, 300, 700)
+    contexto.fillStyle = "rgba(100,0,0,1"
+}
