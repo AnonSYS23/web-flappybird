0d4641d8d82255c937a0f3877501a8063951c390
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-16 20:07:26 -0500

    Recreating the code

--- js/game.js
+++ js/game.js
@@ -1,19 +1,12 @@
-let context = document.getElementById("gCanvas").getContext("2d")
-context.canvas.width = 300
-context.canvas.height = 700
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
 
-let fps = 60
-
-let bird = {
-    x: 100,
-    y: 150,
-    w: 50,
-    h: 50
-}
-
-setInterval(loop, 1000 / fps)
-
-function loop(){
-    context.clearRect(0, 0, 300, 700)
-    contexto.fillStyle = "rgba(100,0,0,1"
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d") //Used for drawing on the board
 }
